20b0d6724fec4e8429b016ea64e35858dc660fd4
Laís Brum <laisbrme@gmail.com>
2025-06-06 22:41:08 -0300

    :sparkles: feat(produtos): atualizar estilos visuais da tabela de produtos

--- desafio-react/src/pages/produtos/index.js
+++ desafio-react/src/pages/produtos/index.js
@@ -145,7 +145,7 @@ export default function Produtos() {
 			{/* <!-- Tabela --> */}
 			<div className="row mt-3">
 				<div className="col-sm-12 table-responsive-sm">
-					<table className="table table-dark table-hover">
+					<table className="table table table-hover">
 						<thead className="table-warning">
 							<tr>
 								<th>Id</th>
@@ -170,7 +170,7 @@ export default function Produtos() {
 										<button
 											id={produto.id}
 											onClick={editar}
-											className="btn btn-outline-warning btn-sm mr-3"
+											className="btn btn-warning btn-sm mr-3"
 											data-bs-toggle="modal"
 											data-bs-target="#modal-cliente"
 										>
@@ -178,7 +178,7 @@ export default function Produtos() {
 										</button>
 										<button
 											id={produto.id}
-											className="btn btn-outline-warning btn-sm mr-3"
+											className="btn btn-warning btn-sm mr-3"
 											onClick={excluir}
 										>
 											Excluir
