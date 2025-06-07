42708c75d032110303889315cfede88b2dfdf6a2
Laís Brum <laisbrme@gmail.com>
2025-06-06 22:19:52 -0300

    :sparkles: feat(home): adicionar botões estilizados para navegação entre Clientes e Produtos

--- desafio-react/src/pages/home/index.js
+++ desafio-react/src/pages/home/index.js
@@ -1,11 +1,29 @@
 import "./index.css";
+import client from "../../assets/client.png";
+import product from "../../assets/product.png";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
-	return (
-		<>
-			<h1>Home em construção</h1>
-		</>
-	);
+    const navigate = useNavigate();
+
+    return (
+        <>
+			<div className="home-container">
+				<div className="home-button">
+					<button onClick={() => navigate("/produtos")}>
+						<img src={client} alt="client icon" />
+						<span>Cadastrar Produtos</span>
+					</button>
+				</div>
+				<div className="home-button">
+					<button onClick={() => navigate("/clientes")}>
+						<img src={product} alt="product icon" />
+						<span>Cadastrar Clientes</span>
+					</button>
+				</div>
+			</div>
+        </>
+    );
 }
 
 export default Home;
